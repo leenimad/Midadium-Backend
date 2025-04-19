cfecc34e207f45ad1824fd8d9be1d0b35da4bbb1
leenimad <leeni.batta@gmail.com>
2025-04-20 00:33:43 +0300

    backend update for admin role

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv');
-console.log('OPENAI KEY EXISTS?', !!process.env.OPENAI_API_KEY);
+
 const connectDB = require('./config/db');
 const cors = require('cors');
 dotenv.config();
-const tls = require('tls');
-// Connect to MongoDB Atlas
+
 connectDB();
   
 const app = express();
@@ -15,20 +14,14 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization']
   })); // Enable CORS
 // Middleware to parse JSON bodies
-app.use(express.json());
-try {
-  tls.DEFAULT_MIN_VERSION = 'TLSv1.3';
-  console.log(`Forcing minimum TLS version to: ${tls.DEFAULT_MIN_VERSION}`);
-} catch (tlsErr) {
-  console.error("Error setting TLS min version:", tlsErr);
-}  
+ 
 // Mount authentication routes at /api/auth
 app.use('/api/auth', require('./routes/authRoutes'));
 // Mount admin routes (or other role-specific routes)
 app.use('/api/admin', require('./routes/adminRoutes'));
 // (Optional) You can add lesson routes similarly when needed:
 // app.use('/api/lessons', require('./routes/lessonRoutes'));
-app.use('/api/lessons', require('./routes/lessonRoutes'))
+//app.use('/api/lessons', require('./routes/lessonRoutes'))
 // Mount teacher routes
 app.use('/api/teacher', require('./routes/teacherRoutes'));
 // Mount student routes
